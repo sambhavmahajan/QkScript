c89b9a3d4a78ffbcc69de2d1f0a83f6cd193a6a1
sambhavmahajan <sambhav_mahajan@outlook.com>
2024-06-30 14:36:08 +0530

    Variable Support

--- main.cpp
+++ main.cpp
@@ -3,31 +3,76 @@
 #include <unordered_map>
 using namespace std;
 
-void parseLine(const string &s);
+void trim(string&);
+void convertVarToVal(string&);
+void cmdPrint(string&);
+void cmdRepeat(string&);
+void cmdVar(string&);
+void parseLine(string&);
 
-typedef void (*CommandFunc)(const string&);
-void cmdPrint(const string& arg){
+//helper functions
+void trim(string &s){
+    int start = 0, end = s.size() - 1;
+    int prevS, prevE;
+    while(start <= end && (s[start] == ' ' || s[start] == '\t')){
+        ++start;
+    }
+    
+    while(end >= start && (s[end] == ' ' || s[end] == '\t')){
+        --end;
+    }
+    s = s.substr(start, end - start + 1);
+}
+
+unordered_map<string,string> varMap;
+void convertVarToVal(string &s){
+    for(auto it : varMap){
+        size_t pos = 0;
+        while((pos = s.find('$' + it.first, pos)) != string::npos){
+            s.replace(pos, it.first.size() + 1, it.second);
+            pos += it.second.size();
+        }
+    }
+}
+
+typedef void (*CommandFunc)(string&);
+
+unordered_map<string, CommandFunc> commandMap = {
+    {"print", cmdPrint},
+	{"repeat", cmdRepeat},
+    {"var", cmdVar}
+};
+
+
+//command functions
+void cmdPrint(string& arg){
+    convertVarToVal(arg);
     cout << arg << '\n';
 }
-void cmdRepeat(const string& arg){
+void cmdRepeat(string& arg){
 	int intSize = arg.find(' ');
 	int i = stoi(arg.substr(0,intSize));
-	const string s = arg.substr(intSize + 1);
+	string s = arg.substr(intSize + 1);
 	while(i-- > 0){
 		parseLine(s);
 	}
 }
-unordered_map<string, CommandFunc> commandMap = {
-    {"print", cmdPrint},
-	{"repeat", cmdRepeat}
-};
+void cmdVar(string &arg){
+    int idx = arg.find(' ');
+    string varName = arg.substr(0, idx);
+    idx = arg.find('=') + 1;
+    string varValue = arg.substr(idx);
+    trim(varValue);
+    varMap[varName] = varValue;
+}
 
-void parseLine(const string &s) {
+void parseLine(string &s) {
     auto spacePos = s.find(' ');
     if (spacePos != string::npos) {
         string command = s.substr(0, spacePos);
         if (commandMap.count(command)) {
-            commandMap[command](s.substr(spacePos + 1));
+            string arg = s.substr(spacePos + 1);
+            commandMap[command](arg);
             return;
         }
     }
